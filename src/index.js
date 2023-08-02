dd1851d7246e0c03ab636fc55c17444439345ee2
Yury Butskevich <124175026+YourunB@users.noreply.github.com>
2023-08-02 14:37:15 +0300

    fix: delete - whith reverse

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 module.exports = function reverse (n) {
   n = String(n);
-  if (n[0] == "-") n = n.slice(1) + n[0];
+  if (n[0] == "-") n = n.slice(1);
   n = n.split("").reverse().join("");
   return Number(n);
 }
